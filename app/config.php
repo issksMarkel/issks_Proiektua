b06707d31cc82406929345d31a643d9a2ea5c0e4
EDVOA76 <segurikerurrunekoz@gmail.com>
2025-10-30 19:33:28 +0100

    Errore bat

--- app/config.php
+++ app/config.php
@@ -17,7 +17,6 @@ function validarNAN($nan) {
     $letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
     return $letras[intval(substr($nan, 0, 8)) % 23] === substr($nan, 9, 1);
 }
-<<<<<<< HEAD
 
 $db_host = 'localhost';
 $db_user = 'root';
@@ -35,6 +34,3 @@ try {
     die("Errorea konexioan: " . $e->getMessage());
 }
 ?>
-=======
-?>
->>>>>>> bbd0d26b0a922afa64144c98ab2631935f560bd9
