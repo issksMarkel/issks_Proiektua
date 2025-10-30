cb8eea3f1c2ca385368911b410524ecd1145e5e9
EDVOA76 <segurikerurrunekoz@gmail.com>
2025-10-30 20:20:41 +0100

    Errorea

--- app/register.php
+++ app/register.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['register'])) {
     $nan = mysqli_real_escape_string($conn, $_POST['nan']);
     
     if (validarNAN($nan)) {
-        $check = mysqli_query($conn, "SELECT id FROM usuarios WHERE nan = '$nan'");
+        $check = mysqli_query($conn, "SELECT id FROM erabiltzaile WHERE nan = '$nan'");
         
         if (mysqli_num_rows($check) > 0) {
             $register_error = "NAN hau dagoeneko erregistratuta dago";
